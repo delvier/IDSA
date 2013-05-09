dda0737a9d73bd2587efe1d443d2494e932f8c18
pljanotx <patrykX.l.janota@intel.com>
2013-05-09 10:23:32 +0200

    Add class Enums,
    Add main CsvCompany enum - used to define collumn in csv.company raw data.

---
+++ CsvView/CsvEnums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsvReaderModule
+{
+    class CsvEnums
+    {
+        enum Company
+        {
+            Id,
+            ColumnB,
+            Name,
+            Shortcut,
+            ColumnE,
+            ColumnF,
+            Date,
+            Description,
+            ColumnI,
+            ColumnJ,
+            ColumnK,
+            ColumnL,
+            Href,
+            PhoneNumber,
+            Email,
+            FullName,
+            HeadAccount,
+            Profile,
+            Addrees,
+            HrefStatus,
+            ShareNumbers,
+            Date2,
+            ColumnW,
+            ColumnX
+        }
+    }
+}
