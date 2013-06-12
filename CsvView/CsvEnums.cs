1143db53f7bd8fee384e1fe3eff085d5024e9223
delvier <pat129@wp.pl>
2013-06-13 00:57:47 +0200

    Csv Enum BIG Data Decode. Still need to translate although on good track.
    Also want to optimalize somehow this enum. I think of static class that will consist of other enums that will be seperate on the segments, cause it's flustrating to handle such big enums...
    
    Morover DB soon be reorganize so we want to have better background prepare for easy handling :)
    
    Cheers.

--- CsvView/CsvEnums.cs
+++ CsvView/CsvEnums.cs
@@ -15,41 +15,47 @@
             ColumnC,    //3
             Year,       //4
             Quater,     //5
-            ColumnF,    //6
-            ColumnG,    //7
-            ColumnH,    //8
-            ColumnI,    //9
-            ColumnJ,    //10
-            ColumnK,    //11
-            ColumnL,    //12
-            ColumnM,    //13
-            ColumnN,    //14
-            ColumnO,    //15
-            ColumnP,    //16
-            ColumnQ,    //17
-            ColumnR,    //18
-            ColumnS,    //19
-            ColumnT,    //20
-            ColumnU,    //21
-            ColumnV,    //22
-            ColumnW,    //23
-            ColumnX,    //24
-            ColumnY,    //25
-            ColumnZ,    //26
-            ColumnAA,   //27
-            ColumnAB,   //28
-            ColumnAC,   //29
-            ColumnAD,   //30
-            ColumnAE,   //31
-            ColumnAF,   //32
-            ColumnAG,   //33
-            ColumnAH,   //34
-            ColumnAI,   //35
-            ColumnAJ,   //36
-            ColumnAK,   //37
-            ColumnAL,   //38
-            ColumnAM,   //39
-            ColumnAN,   //40
+            ColumnF,    //6  //AKTYWA OGOLEM - WIDOK (UKRYJ SZCZEGOLY)
+            ColumnG,    //7  //PASYWA OGOLEM - WIDOK (UKRYJ SZCZEGOLY)
+
+            ColumnH,    //8  //AKTYWA TRWAŁE
+            ColumnI,    //9  //Wartosci Niematerialne i Prawne WNiP
+            ColumnJ,    //10 //Rzeczowe składniki majątku trwałego
+            ColumnK,    //11 //!* Należnosci długoterminowe
+            ColumnL,    //12 //!* Inwestycje długoterminowe
+            ColumnM,    //13 //Pozostałe aktywa trwałe
+
+            ColumnN,    //14 //AKTYWA OBROTOWE
+            ColumnO,    //15 //Zapasy
+            ColumnP,    //16 //Naleznosci krótkoterminowe
+            ColumnQ,    //17 //Inwestycje krótkoterminowe
+            ColumnR,    //18 //!* środki pieniężne
+            ColumnS,    //19 //Pozostałe aktywa obrotowe
+            ColumnT,    //20 //Aktywa przeznaczone do sprzedaży
+
+            ColumnU,    //21 //KAPITAŁ WŁASNY
+            ColumnV,    //22 //kapitał fundusz podstawowy
+            ColumnW,    //23 //Udział (akcje) własne (-)
+            ColumnX,    //24 //Kapitał fundusz zapasowy
+            ColumnY,    //25  ? 0
+            ColumnZ,    //26  ? 0
+            ColumnAA,   //27 //Udziały niekontrolujące
+
+            ColumnAB,   //28 //ZOBOWIĄZANIA DŁUGOTERMINOWE
+            ColumnAC,   //29 //Z tytułu dostaw i usług
+            ColumnAD,   //30 //!* Kredyty i pożyczki
+            ColumnAE,   //31  ? 0
+            ColumnAF,   //32 //!* Inne finansowe zob. długoterminowe
+            ColumnAG,   //33 //Inne zobowiązania długoterminowe
+
+            ColumnAH,   //34 //ZOBOWIĄZANIA KRÓTKOTERMINOWE
+            ColumnAI,   //35 //Z tytułu dostaw i usług
+            ColumnAJ,   //36 //!*Kredyty i pożyczki
+            ColumnAK,   //37  ? 0
+            ColumnAL,   //38 //!*Inne finanoswe zob. krótkoterminowe
+            ColumnAM,   //39 //Inne zobowiązania krótkoterminowe.
+            ColumnAN,   //40  ? 0
+
             Sales,                     //AO //RZiS //41
             OwnSaleCosts,              //AP        //42
             SalesCost1,                //AQ - Sum. //43
@@ -67,24 +73,27 @@
             DiscontinuedOperations,    //BC        //55
             NetProfit,                 //BD        //56
             NetParentProfit,           //BE        //57
-            ColumnBF, //? where is EBITDA          //58
-            ColumnBG, //?                          //59
-            ColumnBH,   //60
-            ColumnBI,   //61
-            ColumnBJ,   //62
-            ColumnBK,   //63
-            ColumnBL,   //64
-            ColumnBM,   //65
-            ColumnBN,   //66
-            ColumnBO,   //67
-            ColumnBP,   //68
-            ColumnBQ,   //69
-            ColumnBR,   //70  
-            ColumnBS,   //71 
-            ColumnBT,   //72  
-            ColumnBU,   //73 
-            ColumnBV,   //74  
-            ColumnBW,   //75  
+
+            //RACHUNEK CASH FLOW
+            ColumnBF,                  //BF        //58 //Przepływy pieniężne z działaności operacyjnej
+            ColumnBG, //?                          //59 //Amortyzacja.
+            ColumnBH,   //60 //Zmiana stanu należności ?
+            ColumnBI,   //61 //Zmiana stanu zobowiązań ?
+            ColumnBJ,   //62 //Zmiana rezerw i pozostałe ?
+            ColumnBK,   //63 //Kapitał obrotowy ?
+            ColumnBL,   //64 ? hm dziura...
+            ColumnBM,   //65 !* Przepływy pieniezne z działanosci inwestycyjnej
+            ColumnBN,   //66 !* CAPEX (niematerialne i rzeczowe)
+            ColumnBO,   //67 !* Przepływy z działalności finansowej.
+            ColumnBP,   //68 ? Emisja akcji
+            ColumnBQ,   //69 ? Kredyty i pożyczki uzyskane.
+            ColumnBR,   //70 ? Spłata kredytów i pożyczek
+            ColumnBS,   //71 ? Zmiana zadłużenia
+            ColumnBT,   //72 Dywidenda
+            ColumnBU,   //73 !* PRZEPŁYWY PIENIĘŻNE RAZEM
+            //DATY
+            ColumnBV,   //74  Sytuacja finansowa na dzień.
+            ColumnBW,   //75  Raport wydany dnia. (KEY FOR SORT!)
             ColumnBX    //76
         }
 
