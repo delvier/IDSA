ae2b0f566ab65919dbc332811c7a8702026cc55f
pljanotX <patrykx.l.janota@intel.com>
2013-06-17 15:47:04 +0200

    First DataScanner Architecture commits

---
+++ CsvView/Presenters/DataScannerPresenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IDSA.Views;
+using IDSA.Models.Repository;
+
+namespace IDSA.Presenters
+{
+    public class DataScannerPresenter
+    {
+        DataScanner view;
+        private readonly IUnitOfWork dbModel;
+        //cached data , easy to handle created once treat as db
+    }
+}
