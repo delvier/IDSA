ede14c0bd527369f4ce2d7e9de5b4fb1dc56eb26
pljanotX <patrykx.l.janota@intel.com>
2013-06-27 13:32:23 +0200

    CachedContainer. Try architecture build for cach company holder which will ease the sorting and calculation operations.

---
+++ CsvView/Models/DataStruct/SortableCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace IDSA.Models.DataStruct
+{
+    public class SortableCompany : Company
+    {
+        public void SortReports() // simple reports sort.
+        {
+            base.Reports.OrderBy(r => r.Year).ThenByDescending(r => r.Quarter);
+        }
+    }
+}
---
+++ CsvView/Modules/CachedListContainer/ICachedContainer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IDSA.Models;
+
+namespace IDSA.Modules.CachedListContainer
+{
+    public interface ICachedContainer
+    {
+
+    }
+
+    public class CompanyCachedContainer : ICollection<Company>
+    {
+        public void Add(Company item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Company item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Company[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(Company item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Company> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
