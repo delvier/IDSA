a9f2d692d853ca8129c40ae953f3e1111e8c90e2
pljanotX <patrykx.l.janota@intel.com>
2013-06-28 15:10:04 +0200

    CalculateToQuarter. Layout.

--- CsvView/Modules/DataCalculation/ReportDataCalculation.cs
+++ CsvView/Modules/DataCalculation/ReportDataCalculation.cs
@@ -8,7 +8,11 @@ namespace IDSA.Modules.DataCalculation
 {
     class ReportDataCalculation : DataCalculation<Report>
     {
-        
+        public IList<Report> CalculateToQuraterReport(IList<Report>)
+        {
+            var res = new List<Report>();
+            return res;
+        }
         public override void CalculationPerform()
         {
             throw new NotImplementedException();
