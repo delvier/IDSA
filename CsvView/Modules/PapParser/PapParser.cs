31d4d304f08f977dd95b6536cafe3eda968b6289
CezaryCezary <CezaryWronka@gmail.com>
2013-08-25 16:56:08 +0200

    [SortOfBigCommit] Move pap parser functionality to new files. Add functionality

---
+++ CsvView/Modules/PapParser/PapParser.cs
@@ -0,0 +1,109 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDSA.Modules.PapParser
+{
+    public class PapParser
+    {
+        #region Fields
+        private List<ReportFields> _reportFields;
+        #endregion
+
+        #region Ctors
+        public PapParser(int reportId)
+        {
+
+            InitializeReportFields();
+
+            takeTodaysReports("2013,8,23");
+            parseReport(230737);
+        }
+        #endregion
+
+        #region Public Methods
+        public HtmlNode takeTodaysReports(string specificDay)
+        {
+            specificDay = "2013,8,23";  //default 0,0,0, -> shows the newest adding reports date
+            HtmlWeb hw = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument page;
+
+            page = hw.Load(@"http://biznes.pap.pl/NSE/pl/reports/espi/term," + specificDay + ",1");
+            //tabela roczna wszystkich raportow     table[1]/tr[x]-ki
+            var data = page.DocumentNode.SelectSingleNode("/html[1]/body[1]/div[1]/div[12]/div[1]/div[7]");
+            //tabela raportow z danego dnia
+            var data1 = page.DocumentNode.SelectSingleNode("/html[1]/body[1]/div[1]/div[12]/div[1]/div[8]");
+
+            return page.DocumentNode;
+        }
+
+        public void parseReport(int reportId)
+        {
+            HtmlWeb hw = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument page = hw.Load(@"http://biznes.pap.pl/NSE/pl/reports/espi/view/" + reportId.ToString());
+            
+            // /tr[x] (numOfElements-1)/2
+            var daneTemp = page.DocumentNode.SelectSingleNode("/html[1]/span[1]/table[5]/tr[1]/td[1]/table[1]");
+
+            var daneFinansowe = page.DocumentNode.ChildNodes["HTML"].ChildNodes["SPAN"]
+                .ChildNodes[25].ChildNodes["TR"].ChildNodes["TD"].ChildNodes["table"];
+            var dane = daneFinansowe.ChildNodes.Elements("TR");
+            foreach (var row in daneFinansowe.Descendants("TR"))
+            {
+                //if (row. isHeader(0-3))
+                {
+
+                }
+                //first row = header
+                // row[2] WYBRANE DANE FINANSOWE
+                // row[3] tys. mln.
+                // row[4] zl. EUR
+                foreach (var item in row.Descendants("TD"))
+                {
+
+                }
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private void InitializeReportFields()
+        {
+            // 29 fields
+            _reportFields = new List<ReportFields>();
+            _reportFields.Add(new AssetsPrimary());
+            _reportFields.Add(new LiabilitiesPrimary());
+            _reportFields.Add(new FixedAssets());
+            _reportFields.Add(new IntangibleAssets());
+            _reportFields.Add(new TangibleFixedAssets());
+            _reportFields.Add(new LongTermReceivablesFixA());
+            _reportFields.Add(new LongTermInvestmentFixA());
+            _reportFields.Add(new OtherFixedAssets());
+            _reportFields.Add(new CurrentAssets());
+            _reportFields.Add(new Inventory());
+            _reportFields.Add(new LongTermReceivablesCurA());
+            _reportFields.Add(new LongTermInvestmentCurA());
+            _reportFields.Add(new Cash());
+            _reportFields.Add(new OtherCurentAssets());
+            _reportFields.Add(new AssetsForSale());
+            _reportFields.Add(new Equity());
+            _reportFields.Add(new CapitalMasterFund());
+            _reportFields.Add(new ShareOfTreasuryStock());
+            _reportFields.Add(new CapitalreserveFund());
+            _reportFields.Add(new NonControllingInterests());
+            _reportFields.Add(new LongTermLiabilities());
+            _reportFields.Add(new SuppliesAndServicesLT());
+            _reportFields.Add(new LoansAndAdvancesLT());
+            _reportFields.Add(new OtherFinancialLT());
+            _reportFields.Add(new OtherLT());
+            _reportFields.Add(new ShortTermLiabilities());
+            _reportFields.Add(new SuppliesAndServicesST());
+            _reportFields.Add(new LoansAndAdvancesST());
+            _reportFields.Add(new OtherFinancialST());
+            _reportFields.Add(new OtherST());
+        }
+        #endregion
+    }
+}
---
+++ CsvView/Modules/PapParser/ReportFieldsNames.cs
@@ -0,0 +1,387 @@
+﻿using System.Collections.Generic;
+
+namespace IDSA.Modules.PapParser
+{
+    public interface ReportFields
+    {
+        List<string> Names { get; }
+        long Value { get; set; }
+    }
+
+
+    //values.Add("Przychody ze sprzedaży", 0);
+    //values.Add("Strata operacyjna", 0);
+    //values.Add("Strata przed opodatkowaniem", 0);
+    //values.Add("Przychody z tytułu odsetek", 0);
+    //values.Add("Przychody z tytułu prowizji", 0);
+    //values.Add("Zysk (strata) brutto", 0);
+    //values.Add("Zysk (strata) netto", 0);
+    //values.Add("Całkowite dochody", 0);
+    //values.Add("Zmiana stanu środków pieniężnych", 0);
+
+    public class AssetsPrimary : ReportFields         //BalanceDataKey 5
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public AssetsPrimary()
+        {
+            Names = new List<string> { "Aktywa razem", "Aktywa ogolem",
+                                        "A k t y w a r a z e m"};
+        }
+
+        public void addName(string additionalName)
+        {
+            Names.Add(additionalName);
+        }
+    }
+
+    public class LiabilitiesPrimary : ReportFields    //BalanceDataKey 6
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LiabilitiesPrimary()
+        {
+            Names = new List<string> { "Pasywa razem", "Pasywa ogolem",
+                                        "P a s y w a r a z e m"};
+        }
+    }
+
+    public class FixedAssets : ReportFields           //BalanceDataKey 7
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public FixedAssets()
+        {
+            Names = new List<string> { "Aktywa trwale" };
+        }
+    }
+
+    public class IntangibleAssets : ReportFields      //BalanceDataKey 8
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public IntangibleAssets()
+        {
+            Names = new List<string> { "Wartosci niematerialne", 
+                                        "Wartosci niematerialne i prawne",
+                                        "Wartosci niematerialne i prawne, w tym:" };
+        }
+    }
+
+    public class TangibleFixedAssets : ReportFields   //BalanceDataKey 9
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public TangibleFixedAssets()
+        {
+            ////Rzeczowe składniki majątku trwałego
+            Names = new List<string> { "Rzeczowe aktywa trwale", 
+                                        "Rzeczowe aktywa trwale:" };
+        }
+    }
+
+    public class LongTermReceivablesFixA : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LongTermReceivablesFixA()
+        {
+            Names = new List<string> { "Naleznosci dlugoterminowe", 
+                                        "Należności handlowe oraz pozostałe należności" };
+        }
+    }
+
+    public class LongTermInvestmentFixA : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LongTermInvestmentFixA()
+        {
+            Names = new List<string> { "Inwestycje dlugoterminowe", 
+                                        "Nieruchomosci inwestycyjne" };
+        }
+    }
+
+    public class OtherFixedAssets : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherFixedAssets()
+        {
+            Names = new List<string> { "Pozostale aktywa dlugoterminowe", 
+                                        "Pozostale dlugoterminowe aktywa finansowe",
+                                        "Pozostałe wartości niematerialne",
+                                        "Pozostałe rozliczenia międzyokresowe",
+                                        "Inne dlugoterminowe aktywa finansowe" };
+        }
+    }
+
+    public class CurrentAssets : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public CurrentAssets()
+        {
+            Names = new List<string> { "Aktywa obrotowe" };
+        }
+    }
+
+    public class Inventory : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public Inventory()
+        {
+            Names = new List<string> { "Zapasy" };
+        }
+    }
+
+    public class LongTermReceivablesCurA : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LongTermReceivablesCurA()
+        {
+            Names = new List<string> { "Naleznosci krotkoterminowe",
+                                        "Naleznosci krótkoterminowe" };
+        }
+    }
+
+    public class LongTermInvestmentCurA : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LongTermInvestmentCurA()
+        {
+            Names = new List<string> { "Inwestycje krotkoterminowe" };
+        }
+    }
+
+    public class Cash : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public Cash()
+        {
+            Names = new List<string> { "Srodki pieniezne i ich ekwiwalenty",
+                                        "srodki pieniezne i inne aktywa pieniezne" };
+        }
+    }
+
+    public class OtherCurentAssets : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherCurentAssets()
+        {
+            Names = new List<string> { "Pozostale aktywa obrotowe" };
+        }
+    }
+
+    public class AssetsForSale : ReportFields //20 //Aktywa przeznaczone do sprzedaży
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public AssetsForSale()
+        {
+            Names = new List<string> { "Aktywa przeznaczone do sprzedazy",
+                                        "Aktywa sklasyfikowane jako przeznaczone do sprzedazy" };
+        }
+    }
+
+    //EQUITY - pasywa
+
+    public class Equity : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public Equity()
+        {
+            Names = new List<string> { "Kapitał własny", "Kapital wlasny" };
+        }
+    }
+
+    public class CapitalMasterFund : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public CapitalMasterFund()
+        {
+            Names = new List<string> { "Kapitał zakładowy" };
+        }
+    }
+
+    public class ShareOfTreasuryStock : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public ShareOfTreasuryStock()
+        {
+            Names = new List<string> { "Udzial akcji wlasnych" };
+        }
+    }
+
+    public class CapitalreserveFund : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public CapitalreserveFund()
+        {
+            Names = new List<string> { "Kapital zapasowy" };
+        }
+    }
+
+    // ColumnY,                   //25  ? 0
+    // ColumnZ,                   //26  ? 0
+
+    public class NonControllingInterests : ReportFields
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public NonControllingInterests()
+        {
+            Names = new List<string> { "Udzialy niekontrolujace" };
+        }
+    }
+
+
+    public class LongTermLiabilities : ReportFields       //28 //ZOBOWIĄZANIA DŁUGOTERMINOWE
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LongTermLiabilities()
+        {
+            Names = new List<string> { "Zobowiazania dlugoterminowe" };
+        }
+    }
+
+    public class SuppliesAndServicesLT : ReportFields     //29 //Z tytułu dostaw i usług
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public SuppliesAndServicesLT()
+        {
+            Names = new List<string> { "Z tytulu dostaw i uslug" };
+        }
+    }
+
+    public class LoansAndAdvancesLT : ReportFields        //30 //!* Kredyty i pożyczki
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LoansAndAdvancesLT()
+        {
+            Names = new List<string> { "Kredyty i pozyczki dlugoterminowe",
+                                        "Kredyty i pozyczki" };
+        }
+    }
+
+    public class OtherFinancialLT : ReportFields        //32 //!* Inne finansowe zob. długoterminowe
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherFinancialLT()
+        {
+            Names = new List<string> { "Inne finansowe zobowiazania dlugoterminowe" };
+        }
+    }
+
+    public class OtherLT : ReportFields        //33 //Inne zobowiązania długoterminowe
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherLT()
+        {
+            Names = new List<string> { "Inne zobowiazania dlugoterminowe" };
+        }
+    }
+
+
+    public class ShortTermLiabilities : ReportFields  //34 //ZOBOWIĄZANIA KRÓTKOTERMINOWE
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public ShortTermLiabilities()
+        {
+            Names = new List<string> { "Zobowiazania krotkoterminowe",
+                                        "Zobowiazania krótkoterminowe" };
+        }
+    }
+
+    public class SuppliesAndServicesST : ReportFields  //35 //Z tytułu dostaw i usług
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public SuppliesAndServicesST()
+        {
+            Names = new List<string> { "Zobowiazania z tytulu dostaw i uslug",
+                                        "Z tytulu dostaw i uslug" };
+        }
+    }
+
+    public class LoansAndAdvancesST : ReportFields  //36 //!*Kredyty i pożyczki
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public LoansAndAdvancesST()
+        {
+            Names = new List<string> { "Krótkoterminowe kredyty i pozyczki",
+                                        "Kredyty i pozyczki" };
+        }
+    }
+
+    //        ColumnAK,                //37  ? 0
+
+    public class OtherFinancialST : ReportFields  //38 //!*Inne finanoswe zob. krótkoterminowe
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherFinancialST()
+        {
+            Names = new List<string> { "Inne finansowe zobowiazania krotkoterminowe" };
+        }
+    }
+
+    public class OtherST : ReportFields  //39 //Inne zobowiązania krótkoterminowe.
+    {
+        public List<string> Names { get; private set; }
+        public long Value { get; set; }
+
+        public OtherST()
+        {
+            Names = new List<string> { "Inne zobowiazania krotkoterminowe" };
+        }
+    }
+
+    //        ColumnAN                //40  ? 0
+
+}
