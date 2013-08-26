a092cbe08ce9c8062b011d22bd674417d82d88b3
CezaryCezary <CezaryWronka@gmail.com>
2013-08-26 21:54:34 +0200

    getReportsFromDate() complete :)

--- CsvView/Modules/PapParser/PapParser.cs
+++ CsvView/Modules/PapParser/PapParser.cs
@@ -3,26 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IDSA.Modules.PapParser
 {
+    public class ReportStructure
+    {
+        public int CompanyId { get { 
+            // TODO: CompanyName.Substring(CompanyName.IndexOf('/'), )
+            return 123; } }
+        public string CompanyName { get; set; }
+        public string CompanyLink { get; set; }
+        public string Link { get; set; }
+        public string Kind { get; set; }
+    }
+
     public class PapParser
     {
         #region Fields
         private HtmlWeb hw;
         private HtmlAgilityPack.HtmlDocument page;
         private List<ReportFields> _reportFields;
-        private Dictionary<string, string> _cmpRepId;
+        private List<ReportStructure> _reportsStruct;
         #endregion
 
         #region Ctors
         public PapParser(int reportId)
         {
             hw = new HtmlWeb();
-
             InitializeReportFields();
-            getTodaysReports(date: new DateTime(2013, 8, 23));
-            parseReport(230737);
+            _reportsStruct = getReportsFromDate(date: new DateTime(2013, 8, 23));
+            foreach (var report in _reportsStruct)
+            {
+                parseReport(report.Link);
+            }
+            //parseReport(230737);
         }
         #endregion
 
@@ -39,19 +54,20 @@ namespace IDSA.Modules.PapParser
             }
         }
 
-        public /*IEnumerable<KeyValuePair<string, string>>*/ void getTodaysReports(DateTime? date)
-        {
+        public List<ReportStructure> getReportsFromDate(DateTime? date)
+        {            
             if (date == null)   //default 0,0,0, -> shows the newest adding reports date
             {
                 date = new DateTime(0, 0, 0);
             }
             page = hw.Load(@"http://biznes.pap.pl/NSE/pl/reports/espi/term," +
                         date.Value.Year + "," + date.Value.Month + "," + date.Value.Day + ",1");
-            
+            List<ReportStructure> reportsStruct = new List<ReportStructure>();
+
             //tabela raportow z danego dnia
             var data = page.DocumentNode.SelectSingleNode("/html[1]/body[1]/div[1]/div[12]/div[1]/div[8]/table[1]");
             var rows = data.Descendants("TR");
-            string XPathCmp = "/html[1]/body[1]/div[1]/div[12]/div[1]/div[8]/table[1]/tr[3]/td[3]/a[1]/b[1]";   ///#text[1]
+            string XPathCmp = "./td[3]/a[1]/b[1]";   ///#text[1]
             for (int i = 2; i < rows.Count(); ++i)
             {
                 var row = rows.ElementAt(i);
@@ -61,40 +77,52 @@ namespace IDSA.Modules.PapParser
                 //    String company;   //row.ChildNode[5] +a href = company link
                 //    String title;     //row.ChildNode[7] + <a href = report link(report ID)
                 //}
-                var t = row.SelectSingleNode("/tr[3]/td[3]/a[1]/b[1]");
-                _cmpRepId.Add(row.SelectSingleNode(XPathCmp).InnerText,
-                    row.ChildNodes[7].InnerHtml); //TODO: poprawic wydobycie linka
-                //yield return new KeyValuePair<string, string>(
-                  //  row.ChildNodes[5].InnerText, row.ChildNodes[7].InnerText);
-                
+                reportsStruct.Add(new ReportStructure
+                {
+                    CompanyLink = row.SelectSingleNode(XPathCmp).ParentNode.Attributes["href"].Value,
+                    CompanyName = row.SelectSingleNode(XPathCmp).InnerText,
+                    Link = row.SelectSingleNode("//td[4]/a[1]").Attributes["href"].Value,
+                    Kind = Regex.Match(row.SelectSingleNode("//td[4]/a[1]").InnerText, 
+                            "[a-zA-Zóżłąę ]+").ToString()
+                });
             }
+            return reportsStruct;
         }
 
         public void parseReport(int reportId)
         {
-            HtmlWeb hw = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument page = hw.Load(@"http://biznes.pap.pl/NSE/pl/reports/espi/view/" + reportId.ToString());
-
+            parseReport("/NSE/pl/reports/espi/view/" + reportId.ToString());
+        }
+        
+        public void parseReport(string innerUrl)
+        {
+            page = hw.Load(@"http://biznes.pap.pl" + innerUrl);
+        
             // /tr[x] (numOfElements-1)/2
-            var daneTemp = page.DocumentNode.SelectSingleNode("/html[1]/span[1]/table[5]/tr[1]/td[1]/table[1]");
+            var data = page.DocumentNode.SelectSingleNode("/html[1]/span[1]/table[5]/tr[1]/td[1]/table[1]");
 
-            var daneFinansowe = page.DocumentNode.ChildNodes["HTML"].ChildNodes["SPAN"]
-                .ChildNodes[25].ChildNodes["TR"].ChildNodes["TD"].ChildNodes["table"];
-            var dane = daneFinansowe.ChildNodes.Elements("TR");
-            foreach (var row in daneFinansowe.Descendants("TR"))
+            //var daneFinansowe = page.DocumentNode.ChildNodes["HTML"].ChildNodes["SPAN"]
+            //    .ChildNodes[25].ChildNodes["TR"].ChildNodes["TD"].ChildNodes["table"];
+            var rows = data.Descendants("TR");
+            
+            var header = data.ChildNodes[1];
+            //if (row. isHeader(0-3))
+            //first row = header
+            // row[2] WYBRANE DANE FINANSOWE
+
+            int factor = 1;
+            if (header.ChildNodes[3].InnerText.Contains("tys."))    // row[3] tys. mln.
             {
-                //if (row. isHeader(0-3))
-                {
+                factor = 1000;
+            }
+            var currency = header.ChildNodes[4].InnerText;// row[4] zl. EUR
+            if (currency.Trim() == string.Empty)
+                currency = "PLN";
 
-                }
-                //first row = header
-                // row[2] WYBRANE DANE FINANSOWE
-                // row[3] tys. mln.
-                // row[4] zl. EUR
-                foreach (var item in row.Descendants("TD"))
-                {
+            for (int i = 3; i < rows.Count(); ++i)
+            {
+                var row = rows.ElementAt(i).Descendants("TD");
 
-                }
             }
         }
         #endregion
