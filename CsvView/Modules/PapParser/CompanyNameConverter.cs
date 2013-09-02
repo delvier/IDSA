f0a88ed109950a156ecb08d222745ab93398c141
CezaryCezary <CezaryWronka@gmail.com>
2013-09-02 17:38:11 +0200

    For testing Company Name Converter

--- CsvView/Modules/PapParser/CompanyNameConverter.cs
+++ CsvView/Modules/PapParser/CompanyNameConverter.cs
@@ -6,16 +6,16 @@ namespace IDSA.Modules.PapParser
     {
         public CompanyNameConverter()
         {
-
+            
         }
 
         public string ConvertToName(int cmpId)
         {
             HtmlWeb hw = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument page = hw.Load(@"http://biznes.pap.pl/pl/reports/espi/term,"
+            HtmlAgilityPack.HtmlDocument page = hw.Load(@"http://biznes.pap.pl/pl/reports/espi/company/"
                 + cmpId.ToString() + ",0,0,1");
             var data = page.DocumentNode.SelectSingleNode
-                ("//a [@href=\"/pl/reports/espi/company/" + cmpId.ToString() + ",0,0,1]\"\b[1]");
+                ("//a [@href=\"/pl/reports/espi/company/" + cmpId.ToString() + ",0,0,0,1]\"\b[1]");
             return data.InnerText;
         }
 
@@ -23,9 +23,14 @@ namespace IDSA.Modules.PapParser
         {
             if (name.Contains(" SA"))
             {
-
+                name = name.Remove(name.Length - 3);
             }
-            return "";
+            return name;
+        }
+
+        public string ConvertToDbId(int cmpId)
+        {
+            return ConvertToDbId(ConvertToName(cmpId));
         }
     }
 }
--- CsvView/Presenters/DataFromHtmlPresenter.cs
+++ CsvView/Presenters/DataFromHtmlPresenter.cs
@@ -38,9 +38,12 @@ namespace IDSA.Presenters
         public string parsePapReports(DateTime? date)
         {
             IReportsCrawler crawler = new ReportsCrawler();
+            var cnc = new CompanyNameConverter();
             int counter = 0;
             foreach (var report in _papParser.parseReportsFromDate(date))
             {
+                string str = cnc.ConvertToDbId(report.CompanyId);
+                //if (_dbModel.Companies.GetAll().Contains(cmp => cmp.))
                 _dbModel.Reports.Add(report);
                 //TODO: Show to user report.CompanyNames
                 ++counter;
